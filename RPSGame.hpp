551e47b50477ba4fb5396f07980bae3eec9f7938
johnsonm325 <johnsonm325@gmail.com>
2017-07-24 22:18:56 -0400

    Updated .hpp file
    First draft changes to .hpp file

--- RPSGame.hpp
+++ RPSGame.hpp
@@ -1,10 +1,31 @@
 #ifndef RPSGAME_HPP
 #define RPSGAME_HPP
 
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
 class RPSGame
 {
+private:
+	int human_wins,
+		computer_wins,
+		ties,
+		rPower = 1,
+		pPower = 1,
+		sPower = 1;
+	char userInput;
+
 public:
 	RPSGame();
 	~RPSGame();
+	void start();
+	void differentStrengths();
+	int setDiffStrength();
+	void playRound();
+	void compChoice();
+	void determineWinner();
 };
 #endif
